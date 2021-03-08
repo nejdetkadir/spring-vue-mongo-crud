13b9f45d920c94b440df2cb5818e5162892c5097
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-08 04:35:21 +0300

    Initial spring boot project with spring initializr

---
+++ src/main/java/com/nejdetkadirbektas/springvuemongocrud/SpringVueMongoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.nejdetkadirbektas.springvuemongocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueMongoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueMongoCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nejdetkadirbektas/springvuemongocrud/SpringVueMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nejdetkadirbektas.springvuemongocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueMongoCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
