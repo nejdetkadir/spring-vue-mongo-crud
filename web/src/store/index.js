c592dfe251a3f0a705a9e1ea837b8a9d9a5cda84
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-08 18:59:46 +0300

    Add axios, edit router and store structure (close #5)

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+axios.defaults.baseURL = process.env.VUE_APP_BASE_PATH || "http://localhost:3000"
+axios.defaults.withCredentials = true
+
+const store = new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {}
 })
+
+export default async function init() {
+  return store
+}
